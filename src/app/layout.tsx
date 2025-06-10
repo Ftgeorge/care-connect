ac28b50d6844112b721d60c1b30712bc1790140b
ftgeorge <ftgeorgean@gmail.com>
2025-06-10 12:57:53 +0100

    feakd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+          integrity="sha512-D7qIE+hzlO7WMPD5mX6ChMBg3AAdwSpDAi8Nnm7Gy29T2CkpNoQxG/89wTiKO64+ljzYY2rPyaEfMZ0w1WLo5Q=="
+        />
+
+      </head>
       <body className="font-sans antialiased">
         {children}
       </body>
